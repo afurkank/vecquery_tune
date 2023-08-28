1dac06bc22aaf735160e430c6a31afc14342fb1b
Ahmet Furkan <62884181+afurkank@users.noreply.github.com>
2023-08-28 19:43:28 +0300

    Delete run_create_database.sh

--- run_create_database.sh
+++
@@ -1,8 +0,0 @@
-torchrun create_database.py \
-    --model_name dbmdz/bert-base-turkish-cased \
-    --model_weights_path model.pt \
-    --data_path data.csv \
-    --max_len 256 \
-    --documents_column document \
-    --metadata_columns metadata \
-    --collection_name collection
