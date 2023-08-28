dbfaad8a01d69f7488074d7d519c294377abdaa1
Ahmet Furkan <62884181+afurkank@users.noreply.github.com>
2023-08-28 19:43:22 +0300

    Delete run_finetune.sh

--- run_finetune.sh
+++
@@ -1,8 +0,0 @@
-torchrun fine_tune.py \
-    --model_name dbmdz/bert-base-turkish-cased \
-    --data_path data.json \
-    --path_to_save_model ./ \
-    --epochs 20 \
-    --batch_size 32 \
-    --max_len 256 \
-    --lr 2e-5
