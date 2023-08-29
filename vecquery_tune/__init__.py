08eff515eebb5ea8c8903795feae714433b107cc
Ahmet Furkan <62884181+afurkank@users.noreply.github.com>
2023-08-29 22:20:29 +0300

    Update __init__.py

--- vecquery_tune/__init__.py
+++ vecquery_tune/__init__.py
@@ -1,3 +1,8 @@
-__version__ = "0.6"
+import os
 
-# Path: __init__.py
+files = os.listdir(__path__[0])
+modules = (
+    x.replace(".py", "") for x in files if x.endswith(".py") and not x.startswith("__")
+)
+for module in modules:
+    __import__("cf_remote." + module)
