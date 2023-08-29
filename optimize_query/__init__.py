9d87fec015b877d7883b777098ade21e0918c7ed
afurkank <furkan.karacikq@gmail.com>
2023-08-29 23:15:25 +0300

    modified:   optimize_query/__init__.py

--- optimize_query/__init__.py
+++ optimize_query/__init__.py
@@ -1,8 +1,1 @@
-import os
-
-files = os.listdir(__path__[0])
-modules = (
-    x.replace(".py", "") for x in files if x.endswith(".py") and not x.startswith("__")
-)
-for module in modules:
-    __import__("optimize_query." + module)
+from .optimize_query import FineTune, Inference, CreateDatabase
