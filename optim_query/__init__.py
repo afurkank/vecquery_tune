89897768df64090f29b6f1909f9728796ce8d526
afurkank <furkan.karacikq@gmail.com>
2023-08-29 23:47:20 +0300

    update

--- optim_query/__init__.py
+++ optim_query/__init__.py
@@ -1,1 +1,11 @@
+from . import (
+    create_database,
+    custom_embedding,
+    fine_tune,
+    inference,
+    loss_funcs,
+    model,
+    optim_query
+)
+
 from .optim_query import FineTune, Inference, CreateDatabase
