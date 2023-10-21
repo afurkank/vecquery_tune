d69d3715b809d09221fff708d48c457826428762
Ahmet Furkan <62884181+afurkank@users.noreply.github.com>
2023-10-22 00:40:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BERTune-Chroma',
-    version='0.1',
+    version='0.0.1',
     description='Fine-tune BERT for vectorized query search',
     url='https://github.com/afurkank/optim_query',
     author='Ahmet Furkan Karacık',
